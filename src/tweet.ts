ffb70b0fba2866a2173823920bba446990e4153b
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-01-20 23:27:03 +0530

    tweetcard inhance

--- src/tweet.ts
+++ src/tweet.ts
@@ -7,5 +7,11 @@ dotenv.config();
 
 export const tweet = async (props: any) => {
   const data = await fetch(props);
-  return tweetcard(data);
+  function validate(data: any) {
+    if (data.errors) {
+      return data.errors[0].detail;
+    }
+    return tweetcard(data);
+  }
+  return validate(data);
 };
--- src/tweetcard.ts
+++ src/tweetcard.ts
@@ -1,10 +1,8 @@
-import "./svg.css";
 export default function tweetcard(props: any) {
   return `
         <svg
-        width="1200"
-        height="420"
-        viewBox="0 0 1200 420"
+        width="850" height="390"
+        viewBox="0 0 850 390"
         fill="none"
         xmlns="http://www.w3.org/2000/svg">
             <rect xmlns="http://www.w3.org/2000/svg" data-testid="card_bg" id="cardBg"
@@ -19,15 +17,37 @@ export default function tweetcard(props: any) {
                     font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
                     text-align: center;
                     margin-top: 20px;
+                    color: #fff;
                 }
                 svg {
                     font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
+                    background: pink;
+                    border-radius: 5px;
+                    border: 5px solid blue;
+                
+                }
+                .tweet {
+                    font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
+                    text-align: center;
+                    max-width: 600px;
+                }
+                text{ 
+                    font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
+                    word-wrap: break-word;
                 }
             </style>
-            <foreignObject x="0" y="0" width="1200" height="50">
-                <h1 xmlns="http://www.w3.org/1999/xhtml" class="header">
-                    Ghulam Yazdani's tweetup
-                </h1>
-            </foreignObject>
+
+            <defs>
+            <rect id="rect" x="50"  y="50" width="80" height="80" rx="80"/>
+            <clipPath id="clip">
+              <use xlink:href="#rect"/>
+            </clipPath>
+          </defs>
+          <use xlink:href="#rect" stroke-width="2" stroke="black"/>
+          <image x="50" y='50' xlink:href="${props.data[0].profile_image_url}" width="80" height="80" clip-path="url(#clip)"/>
+          <text fill="#000000" font-family="Serif" font-size="60" id="svg_3" stroke="#000000" stroke-width="0" text-anchor="middle" transform="matrix(0.844199 0 0 0.844199 18.8536 10.0524)" x="240" xml:space="preserve" y="84.81544">${props.data[0].name}</text>
+  <text fill="#000000" font-family="Serif" font-size="24" id="svg_4" stroke="#000000" stroke-width="0" text-anchor="middle" transform="matrix(1 0 0 1 0 0) matrix(0.675158 0 0 0.675158 39.0932 40.2368)" x="240" xml:space="preserve" y="100.22637">@${props.data[0].username}</text>
+  <text fill="#000000"class="tweet" font-family="Serif" font-size="24" id="svg_5" stroke="#144a9b" stroke-width="0" text-anchor="middle" transform="matrix(0.879186 0 0 1 27.1473 20)" x="411.0082" xml:space="preserve" y="158">lorem isoaskd mslas ,salaselmam, aslmasasacscaasccccsascssacsmasmasksamsmasmmaseksmaskkas kasmaskmca asklacma askasc aksenv kasc</text>
+ 
     </svg>`;
 }
